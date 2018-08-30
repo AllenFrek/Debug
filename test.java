a0163ae6cbe09aa0edd08b75b331bce80fe7853c
Scott <GitScott01@users.noreply.github.com>
2018-08-30 12:21:05 -0400

    Initial commit

---
+++ test.java
@@ -0,0 +1,15 @@
+
+public class test {
+
+	public test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 100; i += 2)
+		{
+		    System.out.println(i);
+		}
+	}
+
+}
